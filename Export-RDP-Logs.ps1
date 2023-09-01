3880aa8d54165f00be381c6cbda3bc41fdbc6ed4
m2021acct <87372879+m2021acct@users.noreply.github.com>
2023-09-01 11:18:57 -0400

    Update Export-RDP-Logs.ps1

--- Export-RDP-Logs.ps1
+++ Export-RDP-Logs.ps1
@@ -3,10 +3,12 @@ $logFolder | Remove-Item –force –Recurse -EA 'SilentlyContinue'
 $($logFolder + '.zip') | Remove-Item –force –Recurse -EA 'SilentlyContinue'
 New-Item -ItemType directory -Path $logFolder -EA 'SilentlyContinue'
 
-get-winevent -logname Security -ErrorAction SilentlyContinue | Format-List -Property * | Out-String | Out-File $($logFolder + '\Security.log') -Append
+# Get specific event IDs
+get-winevent -FilterHashtable @{"ProviderName"="Microsoft-Windows-Security-Auditing";Id=4624,4625,4776} -ErrorAction SilentlyContinue | Format-List -Property * | Out-String | Out-File $($logFolder + '\Security.log') -Append
 get-winevent -logname Microsoft-Windows-RemoteDesktopServices-RdpCoreTS/Operational -ErrorAction SilentlyContinue | Format-List -Property * | Out-String | Out-File $($logFolder + '\RemoteDesktopServices-RdpCoreTS-Operational.log') -Append
 get-winevent -logname Microsoft-Windows-RemoteDesktopServices-SessionServices/Operational -ErrorAction SilentlyContinue | Format-List -Property * | Out-String | Out-File $($logFolder + '\RemoteDesktopServices-SessionServices-Operational.log') -Append
 
+4624 4625 4776
 Start-Sleep 5
 
 Compress-Archive -Force -Path $logFolder -DestinationPath $($logFolder + '.zip')
