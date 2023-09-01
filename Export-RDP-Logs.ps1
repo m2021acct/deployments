4a94927f4be58cffd1175db19a0899ee6bbcaab5
m2021acct <87372879+m2021acct@users.noreply.github.com>
2023-09-01 11:07:26 -0400

    Create Export-RDP-Logs.ps1

---
+++ Export-RDP-Logs.ps1
@@ -0,0 +1,15 @@
+$logFolder = "C:\Windows\Temp\IT-Diagnostics"
+$logFolder | Remove-Item –force –Recurse -EA 'SilentlyContinue'
+$($logFolder + '.zip') | Remove-Item –force –Recurse -EA 'SilentlyContinue'
+New-Item -ItemType directory -Path $logFolder -EA 'SilentlyContinue'
+
+get-winevent -logname Security -ErrorAction SilentlyContinue | Format-List -Property * | Out-String | Out-File $($logFolder + '\Security.log') -Append
+get-winevent -logname Microsoft-Windows-RemoteDesktopServices-RdpCoreTS/Operational -ErrorAction SilentlyContinue | Format-List -Property * | Out-String | Out-File $($logFolder + '\RemoteDesktopServices-RdpCoreTS-Operational.log') -Append
+get-winevent -logname Microsoft-Windows-RemoteDesktopServices-SessionServices/Operational -ErrorAction SilentlyContinue | Format-List -Property * | Out-String | Out-File $($logFolder + '\RemoteDesktopServices-SessionServices-Operational.log') -Append
+
+Start-Sleep 5
+
+Compress-Archive -Force -Path $logFolder -DestinationPath $($logFolder + '.zip')
+# get "C:\Windows\Temp\IT-Diagnostics.zip"
+# $logFolder | Remove-Item –force –Recurse -EA 'SilentlyContinue'
+# $($logFolder + '.zip') | Remove-Item –force –Recurse -EA 'SilentlyContinue'
